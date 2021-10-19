94a34f5673f7c341879803191de93fde6ed95f68
koushikroy-440 <koushikroy295@gmail.com>
2021-10-19 11:41:01 +0530

    download api

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 //* connect database
@@ -14,10 +14,11 @@ dbService();
 
 //* import routes
 const filesRouter = require('./routes/files.route');
+const showRouter = require('./routes/show.route');
 
 //* view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,8 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //* declare all middleware 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/files', filesRouter);
+app.use('/files', showRouter);
 
 //* catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -43,7 +44,7 @@ app.use(function (err, req, res, next) {
 
   //* render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 module.exports = app;
---
+++ routes/show.route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const File = require('../modal/file.modal');
+
+router.get('/:uuid', async (req, res) => {
+    try {
+        const file = await File.findOnce({ uuid: req.params.uuid });
+        if (!file) {
+            return res.render('download', { error: 'Link has been expired. ' });
+        }
+        return res.render('download', {
+            uuid: file.uuid,
+            fileName: file.fileName,
+            fileSize: file.size,
+            download: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+        });
+    } catch (err) {
+        return res.render('download', { error: 'something went wrong' });
+    }
+});
+
+
+module.exports = router;
